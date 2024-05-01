2a8afe01adce32a32eecde3c5139c78c32d0137c
gauri <gauripatil0307@gmail.com>
2024-05-01 11:14:32 +0530

    navbar and sidebar are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import About from './pages/About.jsx';
+import Analytics from './pages/Analytics.jsx';
+import Comment from './pages/Comment.jsx';
+import Product from './pages/Product.jsx';
+import ProductList from './pages/ProductList.jsx';
 
-import './App.css'
 
-function App() {
- 
-
+const App = () => {
   return (
-    <>
-    <h1 className="text-sky-800/100">welcome to reimbursement folder</h1>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/comment" element={<Comment />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+        </Routes>
+      </Sidebar>
+    </BrowserRouter>
+   
+  );
+};
 
-export default App
+export default App;
